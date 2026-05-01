e2d4169877282d076cc921e5bb00f5a90c0ddf32
ogude2dd <ogude2dd@gmail.com>
2026-05-01 22:07:08 +0000

    Add Firebase auth and Firestore integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:the_budget_iq/screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
+import 'firebase_options.dart';
 import 'models/expense.dart';
-import 'widgets/main_scaffold.dart';
 import 'screens/login_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     ChangeNotifierProvider(
       create: (context) => ExpenseStore(),
