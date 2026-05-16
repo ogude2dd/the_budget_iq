40b0ac489ef50674718786e50ac4f1bea9b81b6d
David Ogude <ogude_dd@MacBookAir.lan>
2026-05-16 20:00:01 +0000

    Fix NDK version to use locally installed 30.0.14904198

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.the_budget_iq"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "30.0.14904198"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
