94ccd1921caab3ecd82b172399e3aed4c6bb6622
David Ogude <ogude_dd@MacBookAir.lan>
2026-05-17 09:56:17 +0000

    Add iOS Firebase and Google Sign-In support
    - Add GoogleService-Info.plist for iOS Firebase config
    - Update firebase_options.dart with iOS configuration
    - Add GIDSignIn URL handler in AppDelegate.swift
    - Add REVERSED_CLIENT_ID URL scheme for Google Sign-In

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,16 +1,22 @@
-import Flutter
 import UIKit
+import Flutter
+import GoogleSignIn
 
 @main
-@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
-  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
-    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+  override func application(
+    _ app: UIApplication,
+    open url: URL,
+    options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+  ) -> Bool {
+    return GIDSignIn.sharedInstance.handle(url)
   }
 }
